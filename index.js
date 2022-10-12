0af8bd8afe234dec889330b157a7914be5f23b50
keldibekovaa <115090119+keldibekovaa@users.noreply.github.com>
2022-10-12 13:50:34 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+
+// цикл while
+
+ let i = 1;
+ while (i <= 50){
+  alert(i);
+    i++;
+ }
+
+// цикл for
+
+for (let i = 1; i <= 50; i++) {
+    alert(i)
+}
+
+
+// Задача №2
+let arrone = ["Привет," ,"мир","!"];
+alert(arrone[0] + arrone[1] + arrone[2]);
